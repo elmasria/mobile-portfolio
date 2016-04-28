b4d6f79cecdb69ae0d157002b9dfa58dc30179c4
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2016-04-28 17:46:03 +0300

    fix: Copy images to dist file

--- gruntfile.js
+++ gruntfile.js
@@ -3,20 +3,17 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		htmlmin: {                                     // Task
-    		dist: {                                    // Target
-      			options: {                             // Target options
-        			removeComments: true,
-        			collapseWhitespace: true
-      			},
-      			files: {
-        			'dist/index.html': 'src/index.html',
-        			'dist/project-2048.html': 'src/project-2048.html',
-        			'dist/project-mobile.html': 'src/project-mobile.html',
-        			'dist/project-webperf.html': 'src/project-webperf.html'
-      			}
-  			}
-  		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'dist/index.html': 'src/index.html'
+				}
+			}
+		},
 		uglify: {
 			build: {
 				src: 'src/js/perfmatters.js',
@@ -24,11 +21,11 @@ module.exports = function(grunt) {
 			}
 		},
 		cssmin: {
-  			options: {
-    			shorthandCompacting: false,
-    			roundingPrecision: -1
-  			},
-  			target: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
 				files: [{
 					expand: true,
 					cwd: 'src/css',
@@ -37,12 +34,88 @@ module.exports = function(grunt) {
 					ext: '.min.css'
 				}]
 			}
+		},
+		/*responsive_images: {
+			dev: {
+				options: {
+					engine: 'im',
+					sizes: [{
+						name: "small",
+						width: 120,
+						suffix: '_1x',
+						quality: 120
+					},
+					{
+						name:"normal",
+						width: 350,
+						suffix: '_x1',
+					},
+					{
+						name:"normal",
+						width: 700,
+						suffix:"_x2"
+					},
+					{
+						name: "medium",
+						suffix: "_x1",
+						width: 800
+					},
+					{
+						upscale: true,
+						name: "medium",
+						suffix: "_x2",
+						width: 1600
+					},
+					{
+						name: "small",
+						width: 600,
+						suffix: "_x1"
+					},
+					{
+						name: "small",
+						width: 1200,
+						suffix: "_x2"
+					}]
+				},
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png}'],
+					cwd: 'src/img/',
+					dest: 'dist/img'
+				}]
+			}
+		},*/
+		clean: {
+			dev: {
+				src: ['dist/'],
+			},
+		},
+		mkdir: {
+			dev: {
+				options: {
+					create: ['dist/img']
+				},
+			},
+		},
+		copy: {
+			images: {
+				files: [{
+					expand: true,
+					cwd: 'src/img/',
+					src: ['**/*.{png,jpg,svg}'],
+					dest:'dist/img/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-mkdir');
 
-	grunt.registerTask('default', ['uglify', 'htmlmin', 'cssmin']);
+	grunt.registerTask('default', ['clean', 'uglify', 'htmlmin', 'cssmin', 'mkdir', 'copy']);
 }
